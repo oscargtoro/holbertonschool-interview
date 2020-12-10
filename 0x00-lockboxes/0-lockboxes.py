32ab7a6ecd7a71c2c6c8a98358c6179af3e870d8
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-10 17:46:32 -0500

    Recovered content after requirements passed

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -3,14 +3,28 @@
 '''
 
 
+def unlock(boxes, keys, bOpened):
+    '''Checks if key already used
+    '''
+    for key in keys:
+        if key in bOpened:
+            continue
+        else:
+            bOpened[key] = 'true'
+            unlock(boxes, boxes[key], bOpened)
+
+
 def canUnlockAll(boxes):
     '''Determines if all the boxes can be opened.
-
     Args:
         boxes: list of lists.
-
     Returns:
         True if all boxes can be opened, else returns false
     '''
 
-    pass
+    bOpened = {0: 'true'}
+    unlock(boxes, boxes[0], bOpened)
+    for box in range(len(boxes)):
+        if box not in bOpened:
+            return False
+    return True
