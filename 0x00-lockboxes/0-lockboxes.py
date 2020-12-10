a148ba7d5707b107b4bb4186d408137038d3c757
Oscar Stiven Gomez Toro <oscarstiven87@gmail.com>
2020-12-10 11:27:25 -0500

    Task 0 determines if all the boxes can be opened

---
+++ 0x00-lockboxes/0-lockboxes.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+'''Determines if all the boxes can be opened.
+'''
+
+
+def unlock(boxes, keys, bOpened):
+    for key in keys:
+        if key in bOpened:
+            continue
+        else:
+            bOpened[key] = 'true'
+            unlock(boxes, boxes[key], bOpened)
+
+
+def canUnlockAll(boxes):
+    '''Determines if all the boxes can be opened.
+
+    Args:
+        boxes: list of lists.
+
+    Returns:
+        True if all boxes can be opened, else returns false
+    '''
+
+    bOpened = {0: 'true'}
+    unlock(boxes, boxes[0], bOpened)
+    for box in range(len(boxes)):
+        if box not in bOpened:
+            return False
+    return True
--- 0x00-lockboxes/README.md
+++ 0x00-lockboxes/README.md
@@ -1,1 +1,24 @@
 # 0x00-lockboxes
+
+Determines if **n** boxes can be opened. Each box is numbered sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
+
+- Receives a list of lists
+- A key with the same number as a box opens that box
+- All keys must be positive integers
+
+```
+>>> canUnlockAll = __import__('0-lockboxes').canUnlockAll
+>>>
+>>> boxes = [[1], [2], [3], [4], []]
+>>> print(canUnlockAll(boxes))
+True
+>>>
+>>> boxes = [[1, 4, 6], [2], [0, 4, 1], [5, 6, 2], [3], [4, 1], [6]]
+>>> print(canUnlockAll(boxes))
+True
+>>>
+>>> boxes = [[1, 4], [2], [0, 4, 1], [3], [], [4, 1], [5, 6]]
+>>> print(canUnlockAll(boxes))
+False
+>>>
+```
