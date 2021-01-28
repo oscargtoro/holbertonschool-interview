a2a4e5eb3a7b6c0f3bdfa84c7de53261b03aef28
oscargtoro <oscarstiven87@gmail.com>
2021-01-28 16:01:48 +0000

    removed usage of shlex

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -3,7 +3,6 @@
 '''
 
 import sys
-import shlex
 
 i = 0
 codes = ['200', '301', '400', '401', '403', '404', '405', '500']
@@ -18,19 +17,19 @@ metrics = {'200': 0,
 size = 0
 try:
     for line in sys.stdin:
-        stline = shlex.split(line)
-        if len(stline) == 7:
-            code = stline[5]
+        stline = line.split()
+        if len(stline) == 9:
+            code = stline[7]
             if code.isnumeric() and code in codes:
-                code = stline[5]
                 current = metrics.get(code)
                 metrics.update({code: current + 1})
-                size = size + int(stline[6])
+                size = size + int(stline[8])
         i += 1
         if i % 10 == 0:
             print('File size: {}'.format(size))
             for code in codes:
-                print('{} {}'.format(code, metrics.get(code)))
+                if metrics.get(code) is not 0:
+                    print('{} {}'.format(code, metrics.get(code)))
 except KeyboardInterrupt as e:
     print('File size: {}'.format(size))
     for code in codes:
