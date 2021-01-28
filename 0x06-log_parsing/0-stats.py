0440900122b3924f4997bc7ce6175d6d86cced2c
oscargtoro <oscarstiven87@gmail.com>
2021-01-28 16:15:53 +0000

    prints at the end of stdin

--- 0x06-log_parsing/0-stats.py
+++ 0x06-log_parsing/0-stats.py
@@ -4,35 +4,43 @@
 
 import sys
 
-i = 0
-codes = ['200', '301', '400', '401', '403', '404', '405', '500']
-metrics = {'200': 0,
-           '301': 0,
-           '400': 0,
-           '401': 0,
-           '403': 0,
-           '404': 0,
-           '405': 0,
-           '500': 0}
-size = 0
-try:
-    for line in sys.stdin:
-        stline = line.split()
-        if len(stline) == 9:
-            code = stline[7]
-            if code.isnumeric() and code in codes:
-                current = metrics.get(code)
-                metrics.update({code: current + 1})
-                size = size + int(stline[8])
-        i += 1
-        if i % 10 == 0:
-            print('File size: {}'.format(size))
-            for code in codes:
-                if metrics.get(code) is not 0:
-                    print('{}: {}'.format(code, metrics.get(code)))
-except KeyboardInterrupt:
+
+def data_print(code, size, metrics):
+    '''Handles metrics printing
+    '''
+
     print('File size: {}'.format(size))
     for code in codes:
         if metrics.get(code) is not 0:
             print('{}: {}'.format(code, metrics.get(code)))
-    raise
+
+
+if __name__ == "__main__":
+    i = 0
+    codes = ['200', '301', '400', '401', '403', '404', '405', '500']
+    metrics = {'200': 0,
+               '301': 0,
+               '400': 0,
+               '401': 0,
+               '403': 0,
+               '404': 0,
+               '405': 0,
+               '500': 0}
+    size = 0
+    try:
+        for line in sys.stdin:
+            line.strip('\n')
+            stline = line.split()
+            if len(stline) == 9:
+                code = stline[7]
+                if code.isnumeric() and code in codes:
+                    current = metrics.get(code)
+                    metrics.update({code: current + 1})
+                    size = size + int(stline[8])
+            i += 1
+            if i % 10 == 0:
+                data_print(code, size, metrics)
+        data_print(code, size, metrics)
+    except KeyboardInterrupt:
+        data_print(code, size, metrics)
+        raise
