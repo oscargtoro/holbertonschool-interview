ef99243cfbee1bffb8194d39edd0a5875bbdc283
oscargtoro <oscarstiven87@gmail.com>
2021-02-05 01:57:48 +0000

    Task 0x07. Linked list cycle

---
+++ 0x07-linked_list_cycle/0-check_cycle.c
@@ -0,0 +1,13 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * check_cycle - Computes the sum of two sandpiles.
+ *
+ * @list: 3x3 stable sandpile.
+ * Return: 0
+ */
+int check_cycle(listint_t *list) {
+	printf("%i", list->n);
+	return(0);
+}
