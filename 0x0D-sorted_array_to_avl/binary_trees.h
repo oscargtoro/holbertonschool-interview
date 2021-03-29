fc902e15bf749d43d7b80963b6bae7ba327d09ee
oscargtoro <oscarstiven87@gmail.com>
2021-03-29 03:46:35 +0000

    Betty compliance fixes

--- 0x0D-sorted_array_to_avl/binary_trees.h
+++ 0x0D-sorted_array_to_avl/binary_trees.h
@@ -13,18 +13,16 @@
  */
 struct binary_tree_s
 {
-    int n;
-    struct binary_tree_s *parent;
-    struct binary_tree_s *left;
-    struct binary_tree_s *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
 
 typedef struct binary_tree_s binary_tree_t;
 typedef struct binary_tree_s avl_t;
 
 avl_t *sorted_array_to_avl(int *, size_t);
-/** static int print_t(const binary_tree_t *, int, int, char **);
-* static size_t _height(const binary_tree_t *);*/
 void binary_tree_print(const binary_tree_t *);
 
 #endif
