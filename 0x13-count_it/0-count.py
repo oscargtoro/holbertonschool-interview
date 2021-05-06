23e90f1171b74696853bb2c25ac2e3a4bb6bfb17
oscargtoro <oscarstiven87@gmail.com>
2021-05-06 22:12:02 +0000

    Empty task file

---
+++ 0x13-count_it/0-count.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""Module for the method count_words
+"""
+
+import requests
+
+
+def count_words(subreddit, word_list):
+    """Parses the title of all hot articles in reddit and prints a sorted count
+    of given keywords
+
+    Args.
+        subreddit: name of subreddit to query
+        word_list: list of words to count
+
+    Returns.
+        The count of given keywords
+    """
+    pass
