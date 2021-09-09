4633e573101b7326ad33eb7f3fd17c9bc4980a18
oscargtoro <oscarstiven87@gmail.com>
2021-09-09 21:43:01 +0000

    Added code that makes the method work

--- 0x13-count_it/0-count.py
+++ 0x13-count_it/0-count.py
@@ -5,15 +5,45 @@
 import requests
 
 
-def count_words(subreddit, word_list):
+def count_words(subreddit, word_list, after=None, uniq_list=[], word_count=[]):
     """Parses the title of all hot articles in reddit and prints a sorted count
-    of given keywords
+    of given keywords making use of recursion
 
     Args.
         subreddit: name of subreddit to query
         word_list: list of words to count
 
     Returns.
-        The count of given keywords
+        The count of given keywords, if word_list contains the same word
+        (case-insensitive), the final count should be the sum of each duplicate
     """
-    pass
+    if after == None:
+        word_list = [x.lower() for x in word_list]
+        uniq_list = list(set(word_list))
+        word_count = [0] * len(uniq_list)
+    headers = {'User-Agent': 'python personal test'}
+    parameters = {'after': after}
+    url = 'https://www.reddit.com/r/{}/hot.json'.format(subreddit)
+    r = requests.get(url,
+                     params=parameters,
+                     headers=headers,
+                     allow_redirects=False)
+    if r.status_code != requests.codes.ok:
+        return
+    children = r.json()['data']['children']
+    for post in children:
+        for i in range(len(uniq_list)):
+            title = post['data']['title'].lower().split()
+            word_count[i] += title.count(uniq_list[i].lower())
+    after = r.json()['data']['after']
+    if after != None:
+        count_words(subreddit, word_list, after, uniq_list, word_count)
+    else:
+        uniq_list = [x for _, x in sorted(zip(word_count, uniq_list),
+                                          reverse=True)]
+        word_count = sorted(word_count, reverse=True)
+        for i in range(len(uniq_list)):
+            if word_count[i] != 0:
+                word = uniq_list[i]
+                count = word_count[i] * word_list.count(word)
+                print('{}: {}'.format(word, count))
