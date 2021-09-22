7d224e68de3f31cae17507dab76e6e858af47f64
oscargtoro <oscarstiven87@gmail.com>
2021-09-22 22:35:21 +0000

    Calculates how many square units of water will be retained after it rains

---
+++ 0x10-rain/0-rain.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+
+def rain(walls):
+    """Calculate how many square units of water will be retained after it rains.
+
+    Args.
+        walls: list of non-negative integers.
+
+    Returns.
+    Integer indicating total amount of rainwater retained, 0 on empty list.
+    """
+    n = len(walls)
+    water = 0
+
+    # For every element of walls
+    for i in range(1, n - 1):
+        # Max element on its left
+        left = walls[i]
+        for j in range(i):
+            left = max(left, walls[j])
+
+        # Max element on its right
+        right = walls[i]
+        for k in range(i + 1, n):
+            right = max(right, walls[k])
+
+        # Update the maximum water
+        water += min(left, right) - walls[i]
+
+    return water
