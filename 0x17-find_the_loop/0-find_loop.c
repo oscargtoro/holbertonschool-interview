fae2efeb10fa7615f8879cc0f6ac3acce8365388
oscargtoro <oscarstiven87@gmail.com>
2021-10-05 16:10:32 +0000

    Finds loop in a linked list using a buffer

---
+++ 0x17-find_the_loop/0-find_loop.c
@@ -0,0 +1,30 @@
+#include "lists.h"
+
+/**
+ * find_listint_loop - finds the loop in a linked list.
+ *
+ * @head: Search starting point.
+ *
+ * Return: The address of the node where the loop starts, or NULL if there is
+ * no loop.
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	void *buffer[1024];
+	int n = 0, i = 0;
+
+	if (!head)
+		return (NULL);
+	while (head)
+	{
+		for (i = 0; i < n; i++)
+		{
+			if (head == buffer[i])
+				return (head);
+		}
+		buffer[n] = head;
+		head = head->next;
+		n++;
+	}
+	return (NULL);
+}
