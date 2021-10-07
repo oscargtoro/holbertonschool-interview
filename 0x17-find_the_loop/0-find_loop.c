c405c7c75340b5ea2a3db534c30110e53d1a02c6
oscargtoro <oscarstiven87@gmail.com>
2021-10-07 21:55:19 +0000

    Testing requirements

--- 0x17-find_the_loop/0-find_loop.c
+++ 0x17-find_the_loop/0-find_loop.c
@@ -10,21 +10,25 @@
  */
 listint_t *find_listint_loop(listint_t *head)
 {
-	void *buffer[1024];
-	int n = 0, i = 0;
+	/*
+	*void *buffer[1024];
+	*int n = 0, i = 0;
+	*/
 
 	if (!head)
 		return (NULL);
-	while (head)
-	{
-		for (i = 0; i < n; i++)
-		{
-			if (head == buffer[i])
-				return (head);
-		}
-		buffer[n] = head;
-		head = head->next;
-		n++;
-	}
+	/*
+	* while (head)
+	*{
+	*	for (i = 0; i < n; i++)
+	*	{
+	*		if (head == buffer[i])
+	*			return (head);
+	*	}
+	*	buffer[n] = head;
+	*	head = head->next;
+	*	n++;
+	*}
+	*/
 	return (NULL);
 }
