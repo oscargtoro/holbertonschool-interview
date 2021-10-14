81f69a207690293565ac0bb2f121284d570c794b
oscargtoro <oscarstiven87@gmail.com>
2021-10-14 23:57:34 +0000

    Empty task files

---
+++ 0x18-merge_sort/0-merge_sort.c
@@ -0,0 +1,14 @@
+#include "sort.h"
+
+/**
+ * merge_sort -  sorts an array of integers in ascending order using the Merge
+ * Sort algorithm
+ *
+ * @array: array to sort
+ * @size: size of the array
+ */
+void merge_sort(int *array, size_t size)
+{
+	if (!array || !size)
+		return;
+}
